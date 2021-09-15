0dbbb3bb99eebe4d52b10c1b32f576a5e27992fa
Ashley Pean <pean.ashley@gmail.com>
2021-09-15 12:57:37 -0400

    set up basic route config

---
+++ client/utils/routeConfig.ts
@@ -0,0 +1,14 @@
+const routes = {
+  home: {
+    path: '/',
+    title: 'Home',
+    component: 'Home',
+  }, 
+  auth: {
+    path: '/auth',
+    title: 'Auth',
+    component: 'Auth'
+  }
+}
+
+export default routes;
